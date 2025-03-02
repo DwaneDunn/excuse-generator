fa717691988ac63c3327e0d320dceebe1adc6b5a
Dwane Dunn <dwane.dunn@gmail.com>
2025-03-02 14:12:51 -0500

    Scafold message area

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import './style.css';
 
+import './assets/img/rigo-baby.jpg';
+import './assets/img/4geeks.ico';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log('Hello Rigo from the console!');
 };
+
+// Code from 4Geeks
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = [
+  'before the class',
+  'when I was sleeping',
+  'while I was exercising',
+  'during my lunch',
+  'while I was praying',
+];
